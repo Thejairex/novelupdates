d09b8ae38d25d566f0f2ea89e52e0dc7810b9e08
Thejairex <yairjesus777@gmail.com>
2024-07-20 18:44:48 -0300

    cloudfire attemp skiping

--- scrap.py
+++ scrap.py
@@ -1,4 +1,5 @@
 import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 
 from proxy import Proxies
@@ -6,11 +7,19 @@ from agent import Agent
 
 proxies = Proxies()
 agente = Agent()
+scraper = cloudscraper.create_scraper()
 
 def get_soup(url):
-    response = requests.get(url, proxies=proxies.get_bs4(), headers=agente.get_bs4())
+    response = scraper.get(url, proxies=proxies.get_bs4(), headers=agente.get_bs4())
     return BeautifulSoup(response.content, 'html.parser')
 
 soup = get_soup('https://novelupdates.com/')
 
-print(soup.prettify())
+for i in soup.find_all('a'):
+    print(i.get('href'))
+    
+base = 'https://novelupdates.com/'
+with open('novelupdates.txt', 'w', encoding='utf-8') as file:
+    for link in soup.find_all('a'):
+        if str(link.get("href")).startswith(base):
+            file.write(str(i.get('href')) + '\n')
